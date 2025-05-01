b036a64aac54ee28e429eac190404aa402493005
sonnya <sofi.kravchenko@mail.ru>
2025-05-01 16:53:37 +0300

    dashboard grafana + loki + premetheus + promtail

--- app/main.py
+++ app/main.py
@@ -1,9 +1,36 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
 from db.session import create_tables, delete_tables
 from api.users import router as users_router
 from api.tasks import router as tasks_router
+import logging
+import time
+from prometheus_client import CollectorRegistry, Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
+
+registry = CollectorRegistry()
+
+# Создаем метрики в нашей registry
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP requests",
+    ["method", "endpoint", "http_status"],
+    registry=registry,
+)
+REQUEST_LATENCY = Histogram(
+    "http_request_duration_seconds",
+    "HTTP request latency",
+    ["method", "endpoint"],
+    registry=registry,
+)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,5 +43,44 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root() -> str:
+    return "<3"
+
+
+@app.get("/metrics")
+async def metrics():
+    data = generate_latest(registry)
+    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
+
+
+@app.middleware("http")
+async def metrics_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+
+    REQUEST_COUNT.labels(
+        method=request.method,
+        endpoint=request.url.path,
+        http_status=response.status_code
+    ).inc()
+    REQUEST_LATENCY.labels(
+        method=request.method,
+        endpoint=request.url.path
+    ).observe(process_time)
+
+    logger.info(
+        f"{request.method} {request.url.path} - {response.status_code} - {process_time:.3f}s"
+    )
+    return response
+
+
 app.include_router(users_router)
 app.include_router(tasks_router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
